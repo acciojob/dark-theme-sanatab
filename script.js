4a0ef6319b249fcb04fe8be10c8302e91163efee
sanatab <120128180+sanatab@users.noreply.github.com>
2023-04-05 03:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+const button = document.getElementById("Swap");
+const main = document.getElementById("app");
 
+function toggle(){
+    main.classList.toggle("night");
+    button.classList.toggle("button_night")
+}
+
+button.addEventListener('click' , toggle);
